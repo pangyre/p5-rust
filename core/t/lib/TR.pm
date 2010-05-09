2fb9eb8ee63a6b9334b20251631a9eadcf19e9f2
apv <apv@cassidy.local>
2010-05-08 23:47:35 -0700

    MechCat role added + tweaks.

--- core/t/lib/TR.pm
+++ core/t/lib/TR.pm
@@ -1,7 +1,9 @@
 package TR; # TestRust
 use Rust;
-#with qw( Rust::Role::MechCat );
+with qw( Rust::Role::MechCat );
 use Test::More;
+
+sub catalyst_app { "" }
 
 sub oh_noes : Test(1) {
     my $self = shift;
@@ -17,5 +19,7 @@ __END__
 =pod 
 
  perl -Ilib -It/lib -MTR -e "TR->runtests"
+
+ perl -Ilib -It/lib -I../ext/mech-cat/lib -MTR -e "TR->runtests"
 
 =cut
---
+++ ext/mech-cat/lib/Rust/Role/MechCat.pm
@@ -0,0 +1,94 @@
+package Rust::Role::MechCat;
+use Moose::Role;
+requires qw( catalyst_app );
+
+has "mech" =>
+    isa => "Test::WWW::Mechanize::Catalyst",
+    is => "ro",
+    predicate => "mech_initialized",
+    required => 1,
+    lazy => 1,
+    default => sub {
+        my $self = shift;
+        require Test::WWW::Mechanize::Catalyst;
+        Test::WWW::Mechanize::Catalyst->new(catalyst_app => $self->catalyst_app);
+    },
+    handles => [qw(
+                   get
+                   get_ok
+                   ct
+                   title_is
+                   title_like
+                   content_like
+                   content_contains
+                   text_like
+                   text_contains
+                   follow_link_ok
+                   page_links_ok
+                   uri
+                   status
+                   host
+                   )],
+    ;
+
+# This SHOULD BE an auxiliary method for Test::WWW::Mechanize. Send patch to Andy.
+{
+    package Test::WWW::Mechanize;
+    use warnings;
+    use strict;
+
+    sub text_contains {
+        my ( $self, $target, $desc ) = @_;
+        local $Test::Builder::Level = $Test::Builder::Level + 1;
+        if ( ref($target) eq 'REGEX' ) {
+            diag('content_contains takes a string, not a regex');
+        }
+        $desc = qq{Content contains "$target"} unless defined $desc;
+        contains_string( $self->content(format => "text"), $target, $desc );
+    }
+
+    sub text_like {
+        my ( $self, $regex, $desc ) = @_;
+        $desc = qq{Content is like "$regex"} unless defined $desc;
+        local $Test::Builder::Level = $Test::Builder::Level + 1;
+        like_string( $self->content(format => "text"), $regex, $desc );
+    }
+}
+
+1;
+
+__END__
+
+=pod
+
+=head1 NAME
+
+Testy::Role::MechCat - L<Moose::Role> providing a L<Test::WWW::Mechanize::Catalyst> via the method C<mech>.
+
+=head1 SYNOPSIS
+
+ # something...
+
+=head1 METHODS
+
+=over 4
+
+=item * mech
+
+Readonly, returns a L<Test::WWW::Mechanize::Catalyst> object. The C<mech> object handles methods from L<Test::WWW::Mechanize::Catalyst> like C<get_ok> and C<title_like>. Please refer to that documentation for more.
+
+=head2 New Methods
+
+New methods are injected into the L<Test::WWW::Mechanize> space-
+
+=item * text_contains
+
+=item * text_like
+
+=back
+
+=head1 Copyright and License
+
+See L<Testy>.
+
+=cut
