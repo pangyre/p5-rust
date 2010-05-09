f1c4af85201a62251287722fe12edd046d839f46
apv <apv@cassidy.local>
2010-05-08 21:22:33 -0700

    Some progress.

---
+++ core/t/lib/TR.pm
@@ -0,0 +1,20 @@
+package TR; # TestRust
+use parent "Rust";
+use Test::More;
+
+sub oh_noes : Test(1) {
+    my $self = shift;
+    ok(1, "Ok here");
+    return "...";
+
+}
+
+1;
+
+__END__
+
+=pod 
+
+ perl -Ilib -It/lib -MTR -e "TR->runtests"
+
+=cut
